ab4b6cfd0abcefd7521d05b5ba9644f8dcd21c6b
derrickmo <derrick.mo94@gmail.com>
2023-08-29 17:14:57 +0300

    added register link on login form

--- resources/views/cervical-cancer/view-client-referral.blade.php
+++ resources/views/cervical-cancer/view-client-referral.blade.php
@@ -1,6 +1,8 @@
 <x-content-layout>
     <x-slot name="sub_header_two">
-        <p>cervical cancer screening basic information</p>
+        <div class="bg-gray-200 dark:bg-gray-900 grow py-2 mb-1 rounded-bl-md rounded-br-md uppercase font-bold px-2">
+            Client Cervical Cancer referral
+        </div>
     </x-slot>
     <div class="container">
         <livewire:client-referral-table />
