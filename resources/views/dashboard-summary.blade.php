6b805f7c05ae641022c05d8a02d32e5806c5c1ec
derrickmo <derrick.mo94@gmail.com>
2023-09-02 12:53:59 +0300

    new module card

--- resources/views/dashboard-summary.blade.php
+++ resources/views/dashboard-summary.blade.php
@@ -2,7 +2,10 @@
 
     <div class="container">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-            <div class="p-20 rounded-md bg-gray-100 outline-2 outline-gray-300 shadow-md"></div>
+            <a class="p-4 text-center flex items-center font-extrabold rounded-md bg-orange-400 text-white outline-2 outline-gray-300 shadow-md" href={{route('cervicalCancer.index')}}>
+                CERVICAL CANCER SCREENING
+            </a>
+
             <div class="p-20 rounded-md bg-gray-100 outline-2 outline-gray-300 shadow-md"></div>
             <div class="p-20 rounded-md bg-gray-100 outline-2 outline-gray-300 shadow-md"></div>
             <div class="p-20 rounded-md bg-gray-100 outline-2 outline-gray-300 shadow-md"></div>
